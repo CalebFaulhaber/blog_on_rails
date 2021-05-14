40444f26f0e34aeb44b85c2d4b9b50dd81bc6d9a
caleb.faulhaber@gmail.com <caleb.faulhaber@gmail.com>
2021-05-13 21:12:53 -0700

    created blog_on_rails branch

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,25 @@
+class WelcomeController < ApplicationController
+
+
+
+
+# - create 
+#     - new // refers method in controller
+#         - renders // get request
+#     - create // method
+#         - redirect // post request
+# - read
+#     - index // method
+#         - render // get request
+#     - show // method
+#         - render // get request
+# - update
+#     - edit // method
+#         - render // get request
+#     - update // method
+#         - redirect // patch request
+# - delete
+#     - destroy // method
+#         - redirect // delete request
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+  
+
+
+# - create 
+#   - new // refers method in controller
+#     - renders // get request
+#   - create // method
+#     - redirect // post request
+# - read
+#   - index // method
+#     - render // get request
+#   - show // method
+#     - render // get request
+# - update
+#   - edit // method
+#     - render // get request
+#   - update // method
+#     - redirect // patch request
+# - delete
+#   - destroy(maybe delete) // method
+#     - redirect // delete request
+
+
 end
