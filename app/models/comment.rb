fdb9c0dd9dc144a83dcae0f7bcfa502b376ca324
caleb.faulhaber@gmail.com <caleb.faulhaber@gmail.com>
2021-05-17 04:14:34 -0700

    added css and bootstrap to view files

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,8 +3,8 @@ class Comment < ApplicationRecord
     belongs_to :post
 
     validates :body, 
-    presence: { message: 'Please enter a message' },
-    length: { minimum: 10, message: 'Minimum 10 characters' }
+    presence: { message: 'Please enter a message' }
+    # length: { minimum: 10, message: 'Minimum 10 characters' }
 
     # # PATHS
     # index  - products_path
